3cbc08d2c6cb7ae546a8b04ef6fb00703f012715
Remo de Oliveira Gresta <remoogg@gmail.com>
2021-07-12 00:43:44 -0300

    add first requisition

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  const{ username } = request.body;
+
+  const user = users.find((user) => user.username === username )
+  
+  if(!user){
+    return response.status(400).json({error:"user dont exists"})
+  }
+  request.username = username;
+  return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  
+  const { name, username } = request.body;
+  
+
+  const checkUser = users.some((user) => user.username === username);
+
+  if(checkUser){
+    return response.status(400).json({error: "user already exists"})
+  }
+
+  const user ={
+    name,
+    username,
+    id:uuidv4(),
+    todos:[]
+  }
+  users.push(user);
+  
+  return response.status(201).json(user)
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
